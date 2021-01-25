41188a31d29d87f6155458417a26c69057b86d52
Quintupple <71530975+Quintupple@users.noreply.github.com>
2021-01-25 21:35:51 +0000

    Add files via upload

---
+++ Door.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isOpen;
+    public bool isLocked;
+    public float degrees = 90;
+    public float speed = 10f;
+    public float range = 5;
+    public Camera player;
+    public float totalTime = 6f;
+    public Transform pivot;
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(player.transform.position, player.transform.forward, out hit, range))
+        {
+            if(Input.GetKeyDown(KeyCode.E) && !isOpen)
+            {
+                Debug.Log("open door");
+                
+                StartCoroutine(OpenDoor());
+
+            }
+            else
+            {
+                if(Input.GetKeyDown(KeyCode.E) && isOpen)
+                {
+                    Debug.Log("close door");
+
+                    StartCoroutine(CloseDoor());
+                }
+            }
+        }
+    }
+    
+    IEnumerator OpenDoor()
+    {
+        float elapsedTime = 3;
+        while (elapsedTime < totalTime)
+        {
+            Vector3 openDoor = new Vector3(0, degrees, 0);
+
+            pivot.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, openDoor, Time.deltaTime * speed);
+            elapsedTime += Time.deltaTime;
+
+            isOpen = true;
+            yield return null;
+        }
+    }
+
+    IEnumerator CloseDoor()
+    {
+        float elapsedTime = 3;
+        while (elapsedTime < totalTime)
+        {
+            Vector3 closeDoor = new Vector3(0, 0, 0);
+
+            pivot.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, closeDoor, Time.deltaTime * speed);
+            elapsedTime += Time.deltaTime;
+
+            isOpen = false;
+            yield return null;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(player.transform.position, range);
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    Vector3 currentVelocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    public float jumpHeight = 3f;
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && currentVelocity.y < 0)
+        {
+            currentVelocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            currentVelocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        currentVelocity.y += gravity * Time.deltaTime;
+
+        controller.Move(currentVelocity * Time.deltaTime);
+    }
+}
